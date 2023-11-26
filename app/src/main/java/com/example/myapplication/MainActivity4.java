f8da8ab83fd282b015b6307318e1a73024c50a41
New3 <thinnapatneo.2544@gmail.com>
2023-11-26 21:30:56 +0700

    New3

--- app/src/main/java/com/example/myapplication/MainActivity4.java
+++ app/src/main/java/com/example/myapplication/MainActivity4.java
@@ -7,25 +7,22 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-
-public class MainActivity2 extends AppCompatActivity {
+public class MainActivity4 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
+        setContentView(R.layout.activity_main4);
         Button btnSearch = (Button) findViewById(R.id.button1);
         btnSearch.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(MainActivity2.this, MainActivity3.class);
+                Intent intent = new Intent(MainActivity4.this, MainActivity5.class);
                 Bundle extras = new Bundle();
                 extras.putString("ID","1");
                 startActivity(intent);
 
             }
         });
-
     }
 }
